ad27513f66f596a9fe04a22d08de2bf75d7f36ba
tejaram1234 <96683052+tejaram1234@users.noreply.github.com>
2022-11-03 12:37:04 +0530

    Update sandbox_pattren.java

--- patterns/src/patterns/sandbox_pattren.java
+++ patterns/src/patterns/sandbox_pattren.java
@@ -3,7 +3,25 @@ package patterns;
 public class sandbox_pattren {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=n;i>=1;i--){
+			for(int j=1;j<=n-i;j++){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++){
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n-i;j++){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++){
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
 
 	}
 
